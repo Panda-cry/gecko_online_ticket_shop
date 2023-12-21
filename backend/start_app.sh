8fdf0776e6c739a975e761c073df6a20876425e0
Petar Canic <p.canic@eclecticiq.com>
2023-12-21 22:16:27 +0100

    Updated backend

--- backend/start_app.sh
+++ backend/start_app.sh
@@ -1,7 +1,7 @@
 
 flask db upgrade
-flask run
-#gunicorn -w 4 "app:create_app()" -b 0.0.0.0:8000 --access-logfile=-
+#flask run
+gunicorn -w 4 "app:create_app()" -b 0.0.0.0:8000 --access-logfile=-
 
 
 
